d4cdfc683ddfc5a4c2f72651954aa5787f36c9b8
Tembeon <tembeon@gmail.com>
2022-09-16 15:30:29 +0300

    set home screen to app

--- flutter_isolates/lib/src/core/presentation/app.dart
+++ flutter_isolates/lib/src/core/presentation/app.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import '../../features/home/presentation/home_screem.dart';
+
 class IsolatesApp extends StatelessWidget {
   const IsolatesApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
   }
 }
